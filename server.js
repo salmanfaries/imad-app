cd2dddeb4a170fbb8e8396151234c60959c1520a
salmanfaries <30626380+salmanfaries@users.noreply.github.com>
2017-08-23 13:01:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,60 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+ title:'Article one | Salman Faries',
+ heading: 'Article one',
+ date:'sep 5,2016',
+ content:`<p>Hi, Iam Salman Faries. This is my first HTML and CSS application. Pray for me to become a app developer.
+            </p>
+            <p>
+                I will teach the app to students for sure.Trust me. I will create useful appications for the society.
+            </p>
+            <p> I want develop UNWO app in my style. Help me tpo understand the app development and deployment fully.
+            </p>`
+ 
+};
+function CreateTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=date.content;
+    
+var htmlTemplate=`
+ <html>
+    <head>
+        <title> ${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(CreateTemplate(articleone));
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
